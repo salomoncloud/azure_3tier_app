358a53e9e4d42e9967d0dbdf766318bc7bb76611
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-27 20:05:48 -0400

    Update three_tier_module.tf

--- three_tier_module.tf
+++ three_tier_module.tf
@@ -1,11 +1,11 @@
 module "three_tier_app" {
   source              = "./threetierwebapp/"
-  resource_group_name = "mcitthreetiermodule"
-  location            = "West Europe"
-  admin_username      = var.administrator_login
-  admin_password      = var.administrator_login_password
-  db_admin_username   = var.db_admin_username
-  db_admin_password   = var.db_admin_password
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  db_admin_username   = var.admin_username_db
+  db_admin_password   = var.admin_password_db
 }
 
 output "module_web_vm_public_ip" {
