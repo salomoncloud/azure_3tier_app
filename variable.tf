521214cfd6958d903eb20b10e7273da2d618ca2e
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-27 20:18:05 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,41 @@
+variable "subscription_id" {
+  type = string
+}
 
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
+
+variable "admin_password_db" {
+  type = string
+}
+
+variable "admin_username_db" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+  default = "Mcitterraformrg"
+}
+
+variable "location" {
+  type = string
+  default = "Canada East"
+}
