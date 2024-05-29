986d2c543746d2028cb3360100877d59e9d7130b
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-29 19:34:33 -0400

    Update output.tf

--- threetierwebapp/output.tf
+++ threetierwebapp/output.tf
@@ -1,11 +1,9 @@
 # Output the Public IP of the Web VM
 output "web_vm_public_ip" {
-  count = 7
   value = azurerm_linux_virtual_machine.web_vm.public_ip_address
 }
 
 output "module_web_vm_public_ip" {
-  count = 7
   description = "The public IP address of the web VM"
   value       = azurerm_linux_virtual_machine.web_vm.public_ip_address
 }
